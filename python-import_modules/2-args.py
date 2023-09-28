53432d190ea8db1708bbc43b703ffe01bd720f15
izabela9 <isabelanelaj1@gmail.com>
2023-09-28 11:36:51 +0200

    number of and the list of its arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     from sys import argv
     argv_len = len(argv) - 1
     if argv_len == 0:
-        print("{} arguments.".format(argv_len))
+        print("0 arguments.")
     elif argv_len == 1:
-        print("{} argument:".format(argv_len))
+        print("1 argument:")
     else:
-        print("{} argument:".format(argv_len))
+        print("{} arguments:".format(argv_len))
     for i in range(argv_len):
         print("{}: {}".format(i + 1, argv[i + 1]))
