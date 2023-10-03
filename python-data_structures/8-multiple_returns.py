ec83b067cdc52b04dc8c4969aa6140caacc747a7
izabela9 <isabelanelaj1@gmail.com>
2023-10-03 13:59:30 +0200

    multiple returns

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     first = sentence[0]
-    if len(sentence) == 0:
+    if not sentence:
         return (len(sentence), None)
     return (len(sentence), first)
 
-
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
