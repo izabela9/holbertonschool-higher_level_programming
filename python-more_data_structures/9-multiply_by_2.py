fac4a63cf16f585641299a5483c4272def467124
izabela9 <isabelanelaj1@gmail.com>
2023-10-04 15:32:14 +0200

    unction that returns a new dictionary with all values multiplied by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
     for i in new_dict:
-        new_dict = new_dict[i] * 2
+        new_dict[i] = new_dict[i] * 2
     return new_dict
