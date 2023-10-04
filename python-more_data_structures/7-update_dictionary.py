8afc80bca576b407c52cb0124fb867209fc0a4ba
izabela9 <isabelanelaj1@gmail.com>
2023-10-04 13:59:11 +0200

    function that updates dictionary

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if a_dictionary or key or value is not None:
+        a_dictionary[key] = value
+        return a_dictionary
