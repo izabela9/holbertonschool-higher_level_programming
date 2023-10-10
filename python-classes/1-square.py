90de38a54d860ea2030f1790c1faafca05ab70b1
izabela9 <isabelanelaj1@gmail.com>
2023-10-10 11:58:54 +0200

    Empty square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,11 +3,14 @@
 python3 -c 'print(__import__("my_module").__doc__)'
 python3 -c 'print(__import__("my_module").MyClass.__doc__)'
 """
-
-
 class Square:
     """
     This is the documentation for the Square class.
     The class represents a geometric square and associated operations.
     """
-    pass
+    def __init__(self, size):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        self.__size = size
---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+"""
+class Square:
+    """
+    This is the documentation for the Square class.
+    The class represents a geometric square and associated operations.
+    """
+    def __init__(self, size):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return self.__size ** 2
