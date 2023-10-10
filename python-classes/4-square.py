444880461b38d8204f482bb20f42699ee5daa828
izabela9 <isabelanelaj1@gmail.com>
2023-10-10 12:12:37 +0200

    square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -10,17 +10,13 @@ class Square:
     This is the documentation for the Square class.
     The class represents a geometric square and associated operations.
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """
         python3 -c 'print(__import__("my_module").my_function.__doc__)'
         python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
-        
+
     @property
     def size(self):
         """
