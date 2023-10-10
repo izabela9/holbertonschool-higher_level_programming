a96e25d8735480bd55ae25e3f2f4a1e719efc7e6
izabela9 <isabelanelaj1@gmail.com>
2023-10-10 12:06:47 +0200

    square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,6 +3,8 @@
 python3 -c 'print(__import__("my_module").__doc__)'
 python3 -c 'print(__import__("my_module").MyClass.__doc__)'
 """
+
+
 class Square:
     """
     This is the documentation for the Square class.
--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,6 +3,8 @@
 python3 -c 'print(__import__("my_module").__doc__)'
 python3 -c 'print(__import__("my_module").MyClass.__doc__)'
 """
+
+
 class Square:
     """
     This is the documentation for the Square class.
@@ -18,15 +20,30 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+        
     @property
     def size(self):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
         return self.__size
+    
     @size.setter
     def size(self, value):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
         return self.__size ** 2
