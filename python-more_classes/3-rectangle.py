563133ee1580503df220bd221840e2c9987612bd
izabela9 <isabelanelaj1@gmail.com>
2023-10-12 13:23:59 +0200

    rectangle that finds area and perimeter

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -63,7 +63,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return 0
         return 2*self.width + 2*self.height
-    
+
     def __str__(self):
         hello = ''
         for i in range(self.__height):
