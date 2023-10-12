2d904a2ff52a69c3b82f01621e593274a56d084e
izabela9 <isabelanelaj1@gmail.com>
2023-10-12 11:41:13 +0200

    empty rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -3,6 +3,8 @@
 python3 -c 'print(__import__("my_module").__doc__)'
 python3 -c 'print(__import__("my_module").MyClass.__doc__)'
 """
+
+
 class Rectangle:
     """
     This is the documentation for the Square class.
