336399fe07682c6b2b0702c01e92e9248d73aa2c
izabela9 <isabelanelaj1@gmail.com>
2023-10-12 11:40:01 +0200

    empty rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+"""
 class Rectangle:
+    """
+    This is the documentation for the Square class.
+    The class represents a geometric square and associated operations.
+    """
     pass
