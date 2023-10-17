c7bdbd44942442ddaf2bbe71381b751ac5bdfa01
izabela9 <isabelanelaj1@gmail.com>
2023-10-17 13:17:20 +0200

    available attributes and objects

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
 def lookup(obj):
+    """
+    'print(__import__("my_module").my_function.__doc__)'
+    python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+    """
     list = dir(obj)
     return list
