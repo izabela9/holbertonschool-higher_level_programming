c3cf7fd160b3d88358c7cb42631e9bfedb6a6357
izabela9 <isabelanelaj1@gmail.com>
2023-10-18 15:57:42 +0200

    write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+'''
+Python simple module
+'''
+
+
 def write_file(filename="", text=""):
+    ''''
+    method for writing a file
+    '''
     with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
