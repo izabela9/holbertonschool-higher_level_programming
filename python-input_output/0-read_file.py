3c7853a7bcc81d9ffcde3bca5eca897c8c1601ee
izabela9 <isabelanelaj1@gmail.com>
2023-10-18 14:59:54 +0200

    read a text file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+'''
+Python simple module
+for demonstarative purpose
+'''
+
+
 def read_file(filename=""):
+    ''''
+    method for reading a file
+    '''
     with open(filename, encoding="utf-8") as f:
         read_file = f.read()
         print(read_file)
