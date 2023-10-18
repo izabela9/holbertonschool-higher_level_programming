4a37c1ee6e2bd0d7764fc667b8c3d4ce34dc9ed3
izabela9 <isabelanelaj1@gmail.com>
2023-10-18 16:00:32 +0200

    append to a file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+'''
+Python simple module
+'''
+
+
+def append_write(filename="", text=""):
+    ''''
+    method for writing a file
+    '''
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
