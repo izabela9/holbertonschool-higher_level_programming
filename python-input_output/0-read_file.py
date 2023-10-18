e86dc273a18ffb90e44c1e784b3a00074958fc6a
izabela9 <isabelanelaj1@gmail.com>
2023-10-18 15:36:05 +0200

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -10,4 +10,5 @@ def read_file(filename=""):
     method for reading a file
     '''
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read())
+        for line in f:
+            print(line, end="")
