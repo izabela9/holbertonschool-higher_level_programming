8b3fdf17e6a2c9a1e98d3e061088102bbfaa693a
izabela9 <isabelanelaj1@gmail.com>
2023-10-18 11:54:54 +0200

    my list update

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,16 +8,12 @@ class MyList(list):
     """"
     python3 -c 'print(__import__("my_module").MyClass.__doc__)
     """
-    def __init__(self):
-        """
-        'print(__import__("my_module").my_function.__doc__)'
-        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
-        """
-        return super().__init__()
 
     def print_sorted(self):
         """
         'print(__import__("my_module").my_function.__doc__)'
         python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
         """
-        return super().sort()
+        sorted_list = sorted(self)
+        print(sorted(self))
+        return sorted_list
