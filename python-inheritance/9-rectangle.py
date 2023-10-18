1fed1aacd0e95dedc6139cac181e0cdd25694a18
izabela9 <isabelanelaj1@gmail.com>
2023-10-18 10:44:14 +0200

    full rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''
+    Class that inherits from list class
+    '''
+    def __init__(self, width, height):
+        '''
+        Init function for Rectangle class
+        '''
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        '''
+        Instance method to calculate the area
+        of the rectangle
+        '''
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        String represantation of the rectangle
+        instance
+        """
+        return f"[{self.__class__.__name__}] {self.__width}/{self.__height}"
