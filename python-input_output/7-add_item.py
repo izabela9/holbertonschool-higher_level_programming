b9a397c43d2dd22dd5b15e3f9cfac56e6b0ed9b1
izabela9 <isabelanelaj1@gmail.com>
2023-10-19 15:24:36 +0200

    load, add, save in json file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,11 +9,11 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args = sys.argv
+args = sys.argv[1:]
 filename = "add_item.json"
 my_list = []
 
 with open(filename, 'a+', encoding="utf-8") as f:
-    my_list.extend(args[1:])
+    my_list.extend(args)
     save_to_json_file(my_list, filename)
     load_from_json_file(filename)
