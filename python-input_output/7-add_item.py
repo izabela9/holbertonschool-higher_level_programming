25370337b3d66e7eb3e3ec0521c9cc1b2e214854
izabela9 <isabelanelaj1@gmail.com>
2023-10-20 22:27:28 +0200

    add item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,8 +12,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 args = sys.argv[1:]
 filename = "add_item.json"
 
-with open(filename, 'a+', encoding="utf-8") as f:
+try:
+    my_list = load_from_json_file(filename)
+except Exception:
     my_list = []
-    my_list.extend(args)
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+my_list.extend(args)
+save_to_json_file(my_list, filename)
