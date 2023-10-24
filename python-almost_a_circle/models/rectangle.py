640b6bd3381bcea876f8962b70b0613df1d8f7b1
izabela9 <isabelanelaj1@gmail.com>
2023-10-24 10:52:42 +0200

    update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -87,13 +87,14 @@ class Rectangle(Base):
  ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
 
     def update(self, *args):
-        if len(args) == 1:
-            self.id = args[0]
-        if len(args) == 2:
-            self.width = args[1]
-        if len(args) == 3:
-            self.height = args[2]
-        if len(args) == 4:
-            self.x = args[3]
-        if len(args) == 5:
-            self.y = args[4]
+        for arg in args:
+            if len(args) == 1:
+                self.id = args[0]
+            elif len(args) == 2:
+                self.width = args[1]
+            elif len(args) == 3:
+                self.height = args[2]
+            elif len(args) == 4:
+                self.x = args[3]
+            elif len(args) == 5:
+                self.y = args[4]
