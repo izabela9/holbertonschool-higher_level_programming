0df8ed0765ad7d848d6ca07fa8552bf68352c906
izabela9 <isabelanelaj1@gmail.com>
2023-10-24 12:02:47 +0200

    square class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,14 +5,14 @@ Module for demostrattive purposes
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     '''
     Class for square class
     '''
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-    
+
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.x}/{self.y}\
  - {self.width}"
-  
