acc229096d66639eeaaef25d3909e1c40d4cd99f
izabela9 <isabelanelaj1@gmail.com>
2023-10-25 15:52:48 +0200

    dictionary to instance

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -54,3 +54,16 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''
+        Function that creates an instance
+        from dictionary given
+        '''
+        if cls.__name__ == 'Rectangle':
+            dummy_instance = cls(1, 1)
+        if cls.__name__ == 'Square':
+            dummy_instance = cls(1)
+        dummy_instance.update(**dictionary)
+        return dummy_instance
