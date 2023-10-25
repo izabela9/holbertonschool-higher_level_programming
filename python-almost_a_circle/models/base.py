dee40329c596f38237aca7e1a48da34d5281cc77
izabela9 <isabelanelaj1@gmail.com>
2023-10-25 15:29:39 +0200

    json string to dictionary

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -51,6 +51,6 @@ class Base:
         '''
         function to return the list of the json string representation
         '''
-        if json_string is None:
-            json_string = []
+        if json_string is None or len(json_string) == 0:
+            return []
         return json.loads(json_string)
