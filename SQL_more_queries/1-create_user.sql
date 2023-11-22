c038f39b8b1cbc74ecfd9a5d068f5cdb604069f9
izabela9 <isabelanelaj1@gmail.com>
2023-11-22 06:54:45 -0800

    root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,3 +2,4 @@
 -- because Batch 3 is the best!
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
